73d4cda1c95984c64d92d14291d4cf81f781cb47
r1n <r1n@MacBook-Air-r1n.local>
2026-04-07 22:59:01 +0300

    adding data+parser from vk api

--- scripts/vk_api.py
+++ scripts/vk_api.py
@@ -4,7 +4,7 @@ import csv
 import os
 import json
 
-logging.basicConfig(
+logging.basicConfig( 
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
