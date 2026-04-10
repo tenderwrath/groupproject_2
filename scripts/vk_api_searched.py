42d784d6214df7c7fedb8f843ae79e10b0eef4eb
r1n <r1n@MacBook-Air-r1n.local>
2026-04-10 18:07:10 +0300

    возвращение предыдщуей версии парсера вк

--- scripts/vk_api_searched.py
+++ scripts/vk_api_searched.py
@@ -1,8 +1,8 @@
-import requests # type: ignore
+import requests
+import logging 
 import json
-import logging
 
-logging.basicConfig(
+logging.basicConfig( 
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
@@ -12,29 +12,45 @@ logging.basicConfig(
     ]
 )
 
-token = open("groupproject_2/token.txt").read().strip()
+class vk_api_parser:
+    def __init__(self, token, brand, method):
+        self.token = token
+        self.method = method
+        self.brand = brand
 
-brands = []
+    def delete_stuff_before_domain(self, brand_url):
+        brand_url = brand_url.strip()
+        brand_url = brand_url.replace("https://vk.com/", "")
+        brand_url = brand_url.replace("http://vk.com/", "")
+        brand_url = brand_url.replace("https://vk.ru/", "")
+        brand_url = brand_url.replace("http://vk.ru/", "")
+        brand_url = brand_url.strip("/")
+        brand_url = brand_url.strip()
+        return brand_url
+
+    def save_to_json(self, data, group_domain):
+        filename = "groupproject_2/parsed/" + str(group_domain) + "_searched_wb.json" 
+
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        logging.info(f"Данные сохранены в {filename}")
+
+    def fetch_date(self):
+        logging.info(f"начинаем парсить паблик {self.brand}")
+        group_domain = self.delete_stuff_before_domain(self.brand) 
+        params_for_url = {'access_token': self.token,  'domain':group_domain, 'count': 100, 'query':"WB",'v':'5.199'} #v - версия, обяз параметр 
+        response = requests.get(self.method, params=params_for_url)
+        data = response.json() 
+        self.save_to_json(data, group_domain)
+        logging.info(f"Спарсили {self.brand}")
+ 
+token = open("groupproject_2/token.txt").read()
+brands = [] 
 with open("groupproject_2/brands.txt") as f:
     for link in f:
         link = link.strip()
-        brands.append(link)
-
-for domain in brands:
-    logging.info(f"Ищем посты с WB в: {domain}")
-
-    response = requests.get("https://api.vk.com/method/wall.search", params={
-        'access_token': token,
-        'domain': domain,
-        'query': 'WB',
-        'count': 100,
-        'v': '5.199'
-    })
-
-    data = response.json()
-
-    filename = "groupproject_2/parsed/" + domain + "_searched_wb.json"
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-    logging.info(f"Сохранено в {filename}")
+        brands.append(link) 
+method = "https://api.vk.com/method/wall.search"
+for brand in brands:
+    parser = vk_api_parser(token, brand, method)
+    parser.fetch_date()
